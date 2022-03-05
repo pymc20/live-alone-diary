cb87334c79b5917e6673d93452fd8785d3eebde8
bym <pymc20@naver.com>
2022-03-05 20:12:54 +0900

    jest 추가

---
+++ packages/core/components/presentational/Button.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import styled from "styled-components"
+
+const Button = styled.button`
+  color: green;
+`
+
+const ButtonWrapper = () => {
+    return (
+        <Button></Button>
+    )
+}
+
+export default ButtonWrapper
